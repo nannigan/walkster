777a450a99dd2ec680ce8ba4ac3d9df7f38a1e63
Nan Davenport <nannigan@sonic.net>
2014-09-23 01:16:38 +0000

    try will paginate

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
 	def index
 		@places = Place.all
+		@placess = Place.paginate(:page => params[:page], :order => 'created_at DESC', :per_page => 5)
 	end
 end
