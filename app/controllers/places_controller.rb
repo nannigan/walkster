18aebf77b67577da77c019abbd3ad57a5b8355fa
Nan Davenport <nannigan@sonic.net>
2014-09-23 19:49:07 +0000

    form works to add place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,4 +7,16 @@ class PlacesController < ApplicationController
 	def new
 		@place = Place.new
 	end
+
+	def create
+		Place.create(place_params)
+		redirect_to root_path
+		# can see in rake routes that #index is root
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:name, :description, :address)
+	end
 end
