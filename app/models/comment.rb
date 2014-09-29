e63d0612cfb85913785612af20e602de5b9e6ac1
Nan Davenport <nannigan@sonic.net>
2014-09-29 23:41:55 +0000

    humanize rating

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,18 @@
 class Comment < ActiveRecord::Base
 	belongs_to :place
 	belongs_to :user
+
+
+RATINGS = {
+ 'one star' => '1_star', 
+ 'two stars' => '2_stars', 
+ 'three stars' => '3_stars', 
+ 'four stars' => '4_stars', 
+ 'five stars' => '5_stars' 
+}
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+    # returns 'one star' rather than '1_star'
+  end
 end
